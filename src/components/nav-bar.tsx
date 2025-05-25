7827c46c1b135e35b96beadc5927f3dda6ef29f7
loihnt05 <23520869@gm.uit.edu.vn>
2025-05-25 16:21:30 +0700

    component nav bar

---
+++ src/components/nav-bar.tsx
@@ -0,0 +1,113 @@
+import {FaSpotify} from "react-icons/fa";
+import {GoHomeFill} from "react-icons/go";
+import {IoIosSearch} from "react-icons/io";
+import {TiFolderOpen} from "react-icons/ti";
+import {GrInstallOption} from "react-icons/gr";
+import {useRef} from "react";
+
+function NavBar() {
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleDivFocus = () => {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    };
+    return (
+        <div className="h-1000 bg-blue-300">
+            <div className="bg-black flex w-full sticky top-0 items-stretch">
+                {/*spotify*/}
+                <div className="flex p-5 hover:cursor-pointer">
+                    <a className="flex justify-center items-center" href="#">
+                        <FaSpotify className="text-white text-3xl" title="Spotify"/>
+                    </a>
+                </div>
+                {/*search*/}
+                <div className="flex justify-center gap-2 pr-10 ">
+                    <div className="items-center flex ">
+                        <button className="hover:cursor-pointer bg-neutral-800 p-2 rounded-full flex
+                        hover:bg-neutral-700 transition-colors">
+                            <GoHomeFill className="text-white text-3xl" title="Home"/>
+                        </button>
+                    </div>
+
+                    <div className="flex items-center group ">
+                        <div tabIndex={0} className="flex bg-neutral-800 rounded-full transition-all
+                        group-hover:bg-neutral-700 border-3 group-hover:border-gray-700
+                         border-transparent focus-within:border-white focus-within:border-3
+                         hover:cursor-text"
+                             onFocus={handleDivFocus}>
+                            <div className="items-center flex">
+                                <button className="hover:cursor-pointer p-2 ">
+                                    <IoIosSearch className=" text-gray-400 text-3xl transition-all
+                                    group-hover:text-gray-300 group-focus-within:text-white" title="Search"/>
+                                </button>
+                            </div>
+                            <div className="flex items-center lg:w-100">
+                                <input ref={inputRef} className="focus:outline-none focus: text-white
+                                bg-neutral-800 w-full transition-all group-hover:bg-neutral-700"
+                                       type="text"
+                                       placeholder="What do you want to play?"/>
+                            </div>
+                            <div className="flex items-center">
+                                <div className="flex items-center">
+                                    <button className="border-s-1 px-2 border-gray-400 transition-all
+                                    hover:cursor-pointer">
+                                        <TiFolderOpen className="text-gray-400 transition-all text-2xl "
+                                                      title="Browse"/>
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/*login*/}
+                <div className="flex justify-center gap-2">
+                    <button
+                        className="text-gray-400 font-bold hover:cursor-pointer transition-colors
+                        hover:text-white">Premium
+                    </button>
+                    <button
+                        className="text-gray-400 font-bold hover:cursor-pointer transition-colors
+                        hover:text-white">Support
+                    </button>
+                    <button
+                        className="text-gray-400 font-bold hover:cursor-pointer transition-colors
+                        hover:text-white pr-3">Download
+                    </button>
+                    <div className="flex items-center">
+                        <div className="border-s-1 border-gray-300 h-2/5"></div>
+                    </div>
+                    <div className="flex items-center px-5">
+                        <div className="flex justify-center">
+                            <button className="flex items-center gap-1 text-gray-400
+                            group transition-colors font-bold
+                            hover:cursor-pointer hover:text-white">
+                                <GrInstallOption
+                                    className="font-bold mt-1 text-gray-400 group-hover:text-white
+                                    transition-colors"/>
+                                Install App
+                            </button>
+                        </div>
+                    </div>
+
+                    <div className="flex justify-center gap-2">
+                        <button
+                            className="text-gray-400 font-bold hover:text-white
+                            transition-colors hover:cursor-pointer pr-2">
+                            Sign up
+                        </button>
+                        <div className="flex items-center">
+                            <button className="text-black font-bold px-8 py-3 bg-white
+                            rounded-full flex hover:cursor-pointer hover:opacity-95">Log in
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default NavBar;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {BrowserRouter, Route, Routes} from "react-router";
 import App from "./App.tsx";
+import NavBar from "./components/nav-bar.tsx";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path='/'>
+                  <Route index element={<App/>}></Route>
+                  <Route path="/test" element={<NavBar/>}></Route>
+              </Route>
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
